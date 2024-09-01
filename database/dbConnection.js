8d6a5eda4cd5ed770ebcede01a395f41548f19f3
mohamed atef <mhmd-Atef@github.com>
2024-09-02 00:54:05 +0300

    change db

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -1,6 +1,6 @@
 import { connect } from "mongoose";
 
-export const dbConnection =connect("mongodb+srv://E-commerce:p0fBE5W5sDiv1Qfk@cluster0.nfypn.mongodb.net/E-commerce")
+export const dbConnection =connect("mongodb+srv://medo82852@admin:medo82852@cluster0.nfypn.mongodb.net/E-commerce")
 .then(() => {
     console.log("Database connected successfully");
 });
